e4de0a51df0fbe1dc2653f145ec07f9a88d74dfc
Atharv <atharvnandkishorkulkarni@gmail.com>
2025-01-07 21:08:24 +0530

    Added Structure of  All the components and files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import NavBar from "./components/NavBar/NavBar";
-import {Button} from "./components/ui/button";
+import { Button } from "./components/ui/button";
 
 export default function App() {
-  return (
+  return ( 
     <>
-      <NavBar />
-      <Button>ATHARV</Button>
+      <div>
+        <NavBar />
+      </div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
