ded2a78b8991ad610d9702505cbdd2853ea951a8
Atharv <atharvnandkishorkulkarni@gmail.com>
2025-01-07 18:39:14 +0530

    Second commit (check)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import NavBar from "./components/NavBar/NavBar";
+import {Button} from "./components/ui/button";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <NavBar />
+      <Button>ATHARV</Button>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 }
